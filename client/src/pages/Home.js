326051ccad10dcce80c05b71e1607083bad137b7
raimop <ray@tlu.ee>
2021-04-21 14:41:52 +0300

    Typo fix

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
   return ( 
     <>
     <main className="container">
-      <h1>Sulgpalli väljakutse rakendus</h1> 
+      <h1>Sulgpalli väljakutsete rakendus</h1> 
       <p>Selleks, et esitada väljakutse, on vaja registreerida ja liituda edetabeliga</p>
       <p>Prototüüp valminud bakalaureusetöö raames TLÜ 2021</p>
       <p className="attribution">Logo on teinud <a href="https://www.freepik.com" title="Freepik">Freepik</a> ja saadud aadressit <a href="https://www.flaticon.com/" title="Flaticon">flaticon.com</a></p>
